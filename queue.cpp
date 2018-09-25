1b0315cc2295977accf82f6113a60b9f925976bd
ナイショだよ～ <yohanesadrianevan@gmail.com>
2018-09-24 19:25:19 -0700

    sd remed prtm2

---
+++ queue.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+template <class T>
+class queue
+{
+	T *item;
+	int count;
+public:
+	queue<T>()
+	{
+		count = 0;
+	}
+	void enqueue(T _item)
+	{
+		T *temp = item;
+		item = new T[count + 1];
+		for (int i = 0; i < count; i++)
+		{
+			item[i] = temp[i];
+		}
+		item[count] = _item;
+		count++;
+		delete[] temp;
+	}
+	T dequeue()
+	{
+		T *temp, ret;
+		ret = item[0];
+		item = new T[count - 1];
+		for (int i = 0; i < count; i++)
+		{
+			item[i] = temp[i + 1];
+		}
+		count--;
+		delete[] temp;
+		return ret;
+	}
+	T front()
+	{
+		return item[0];
+	}
+	bool isempty()
+	{
+		return (count <= 0);
+	}
+};
